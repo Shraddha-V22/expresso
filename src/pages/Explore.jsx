b1f237faf56768ecab758d7b8dc618a40592a6fb
Shraddha-V22 <shraddhav1935@gmail.com>
2023-06-25 19:14:33 +0530

    fix: exclude user post

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,16 +1,27 @@
+import { useMemo } from "react";
 import UserPost from "../components/UserPost";
 import { usePosts } from "../contexts/PostsProvider";
+import { useAuth } from "../contexts/AuthProvider";
 
 export default function Explore() {
   const {
     postsData: { posts },
   } = usePosts();
+  const {
+    userData: {
+      user: { userDetails },
+    },
+  } = useAuth();
+
+  const explorePosts = useMemo(() => {
+    return posts.filter(({ username }) => username !== userDetails?.username);
+  }, [posts, userDetails]);
 
   return (
     <section>
       {/* <h1>Explore</h1> */}
       <section className="flex flex-col gap-2">
-        {posts?.map((post) => (
+        {explorePosts?.map((post) => (
           <UserPost key={post._id} userPost={post} />
         ))}
       </section>
