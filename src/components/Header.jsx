e06bd5a0584122e82eab0409859d50ca51a7a1f4
Shraddha-V22 <shraddhav1935@gmail.com>
2023-06-27 22:18:18 +0530

    style: update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
     },
   } = useAuth();
   return (
-    <header className="fixed top-0 z-10 w-full border-b bg-white p-2">
+    <header className="fixed top-0 z-10 flex w-full items-center justify-between bg-white px-2 py-1">
       <button
         onClick={() => setShowMenu((prev) => !prev)}
         className="h-[40px] w-[40px] cursor-pointer overflow-hidden rounded-full border-[1px]"
@@ -21,6 +21,8 @@ export default function Header() {
           className="h-full w-full object-cover"
         />
       </button>
+      <h1 className="text-xl">Expresso</h1>
+      <div className="w-4"></div>
       <Sidebar showMenu={showMenu} setShowMenu={setShowMenu} />
     </header>
   );
