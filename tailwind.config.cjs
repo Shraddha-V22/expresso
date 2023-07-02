2df49f82c9e884d8c7a0aea7060bcf21b60a7353
Shraddha-V22 <shraddhav1935@gmail.com>
2023-07-02 13:49:13 +0530

    style: add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        japnicaLight: "#FCAA95",
+        japnica: "#E7846D",
+        japnicaDark: "#CF6650",
+        japnicaDarker: "#B44B37",
+
+        sanJuanLighter: "#5B7F99",
+        sanJuanLight: "#415E78",
+        sanJuan: "#2E4661",
+        sanJuanLigher: "#213652",
+      },
+      fontFamily: {
+        karla: ["Karla"],
+      },
+    },
   },
   plugins: [],
 };
